dc23df3be8c9610b75b92f1226bd778d25dff9c0
Jared Graham <jgram153@icloud.com>
2025-05-05 18:39:02 -0400

    login force redirtc come

--- next-code/src/app/extension-sign-up/page.tsx
+++ next-code/src/app/extension-sign-up/page.tsx
@@ -42,7 +42,7 @@ export default function ExtensionLoginPage() {
   return (
     <main className="p-6 text-center max-w-sm mx-auto">
       <h1 className="text-2xl font-bold mb-4">Sign in or Sign up</h1>
-      <SignUp />
+      <SignUp forceRedirectUrl={"/"} />
 
       <p className="mt-4 text-sm text-gray-500">
         After signing in, this window will close automatically.
