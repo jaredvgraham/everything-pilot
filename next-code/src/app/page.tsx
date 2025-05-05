28fd909bb49f04ba1c1233a3d52f006f457005a0
Jared Graham <jgram153@icloud.com>
2025-05-05 18:32:07 -0400

    login page and told clerk where the urls are

--- next-code/src/app/page.tsx
+++ next-code/src/app/page.tsx
@@ -1,6 +1,49 @@
 import Image from "next/image";
+import { useEffect } from "react";
+import { useAuth, useSession } from "@clerk/nextjs";
 
 export default function Home() {
+  const { isLoaded, isSignedIn, getToken } = useAuth();
+  const { session } = useSession();
+  console.log("session", session);
+  console.log("isLoaded", isLoaded);
+  console.log("isSignedIn", isSignedIn);
+  useEffect(() => {
+    console.log("in useEffect");
+
+    const sendTokenToExtension = async () => {
+      if (!isLoaded || !isSignedIn) {
+        console.log("not signed in");
+        return;
+      }
+
+      let token = await getToken({ template: "extension" });
+      if (!token && session) {
+        token = await session.getToken();
+      }
+
+      if (!token) {
+        console.log("no token found");
+        return;
+      }
+      console.log("token", token);
+
+      // Send token to extension
+      try {
+        window.opener?.postMessage(
+          { type: "CLERK_EXTENSION_AUTH", token },
+          "*"
+        );
+        console.log("posting token to extension");
+      } catch (error) {
+        console.log("error posting token to extension", error);
+      }
+
+      // window.close(); // Optional auto-close
+    };
+
+    sendTokenToExtension();
+  }, [isLoaded, isSignedIn, session]);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
