5a057b2c3edd8bd445767e51aa395852cfed16fe
Jared Graham <jgram153@icloud.com>
2025-05-12 17:56:22 -0400

    test prod stripe

--- next-code/src/app/page.tsx
+++ next-code/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React, { useState, useEffect } from "react";
+
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import Image from "next/image";
+
 import LandingPage from "@/components/Landing";
 import Dashboard from "@/components/Dashboard";
 
